171a6c2b42b75aabc0982b99c7e4436aa0564ad9
readysetlearn <mister_josh@ymail.com>
2016-06-13 18:56:05 -0700

    Finished implementing and fixed bugs. Things appear to be working. Decided to add a ‘front’ variable to indicate the first node is at index 1 in array. Hopefully fixing the magic number adds clarity to reduce off-by-one errors. Also renamed ‘size’ to ‘end’ to better indicate the way it’s used. Created method isHeap to test if array does in fact satisfy the heap property.

--- BinaryMaxHeap.java
+++ BinaryMaxHeap.java
@@ -1,12 +1,84 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package data.structures;
+
+import java.util.NoSuchElementException;
 
 
 public class BinaryMaxHeap {
-    private int[] heap;
-    private int size;
+    
+    public BinaryMaxHeap(final int MAX_SIZE) {
+        heap = new int[MAX_SIZE + FRONT];//extra element at front simplifies calculations (http://stackoverflow.com/questions/22900388/why-in-a-heap-implemented-by-array-the-index-0-is-left-unused)
+        end = 0;
+        this.MAX_SIZE = MAX_SIZE;
+    }
+    
+    public void insert(final int key) {//in heaps, the value being stored is called the "key"
+        if(end == MAX_SIZE) {
+            throw new NoSuchElementException("heap is full");
+        }
+        
+        heap[++end] = key;
+        //TODO: this sholdn't even need an argument passed
+        heapifyUp(end); 
+        
+
+    }
+    
+    /*INPUT i: index of child*/
+    private int getParentIndx(final int i) {
+        return ((int)Math.floor((i / 2)));//result is automatically truncated to int
+    }
+    
+    /*consider maknig this a public method,
+    so that the user can insert a bunch of keys
+    and only have to heapify once*/
+    private void heapifyUp(int childIndx) {//this function could be written in a recursive way
+        int newKey = heap[childIndx];//this is value that was just added
+        
+        while(childIndx > FRONT && newKey > heap[getParentIndx(childIndx)]) {//not sure if the logic is correct in this line, I think depending on > or < it changes whether it's a max, min or some other type of heap
+            heap[childIndx] = heap[getParentIndx(childIndx)];
+            childIndx = getParentIndx(childIndx);
+        }
+        
+        heap[childIndx] = newKey;
+    }
+    
+    /*display contents of heap, including unused first element*/
+    public void dump() {
+        for(int i = 0; i <= end; i++) {
+            System.out.println("index: " +i+ " key: "+heap[i]);
+         }
+    }
+    
+    public void pprint() {
+        for (int i = FRONT; i <= (int)Math.floor(end / 2); i++) {
+            System.out.print("Parent: " + heap[i] + " Left child: " + heap[2 * i]);
+            if(2 * i + 1 <= end)//must check as parent may have no right child
+                    System.out.print(" Right child: " + heap[2 * i  + 1]);
+            
+            System.out.println();
+        }
+        assert isHeap() : "not a heap";
+    }
+    
+    /*returns true iff array has heap property*/
+    /*consider making this a static method so any array can be tested*/
+    private boolean isHeap() {
+        for(int i = FRONT; i <= Math.ceil(end / 2); i++) {
+            if(2*i+1 <= end && heap[i] < heap[2*i+1])
+                    return false;
+            else if(2*i <= end && heap[i] < heap[2*i])
+                    return false;
+        }
+        
+        return true;
+			
+    }
+
+    
+    
+    
+    private final int[] heap;
+    private int end;//current size
+    private final int MAX_SIZE;//space allocated for heap
+    private final int FRONT = 1;//first key is inserted at index 1, not 0
 }
---
+++ TestHeap.java
@@ -0,0 +1,25 @@
+package data.structures;
+
+
+public class TestHeap {
+
+
+    public static void main(String[] args) {
+        BinaryMaxHeap myHeap = new BinaryMaxHeap(8);
+        
+        myHeap.insert(3);
+        myHeap.insert(4);
+        myHeap.insert(5);
+        myHeap.insert(1);
+        myHeap.insert(10);
+        myHeap.insert(53);
+        myHeap.insert(11);
+        myHeap.insert(2);
+
+        
+
+
+        myHeap.pprint();
+    }
+    
+}
